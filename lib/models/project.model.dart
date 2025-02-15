bc277a1de6418382db8e9a0e8f739e46dc9049ba
Michal Kus <michal513717@gmail.com>
2025-02-15 14:50:36 +0100

    Added basic nav

---
+++ lib/models/project.model.dart
@@ -0,0 +1,8 @@
+
+
+class Project {
+  final String id;
+  final String name;
+
+  Project({ required this.id, required this.name });
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:s_f_a/main.dart';
+import 'package:sfa/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
