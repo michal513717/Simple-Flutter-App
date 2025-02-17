5db6483ee35e5f6f6072583e3349bd0de0679c52
Michal Kus <michal513717@gmail.com>
2025-02-17 14:54:10 +0100

    Added providers

---
+++ lib/providers/task_provider.dart
@@ -0,0 +1,37 @@
+import 'package:localstorage/localstorage.dart';
+import 'package:flutter/material.dart';
+import '../models/task.model.dart';
+import 'dart:convert';
+
+class TaskProvider with ChangeNotifier {
+  LocalStorage storage;
+  List<Task> _tasks = [];
+
+  List<Task> get tasks => _tasks;
+
+  TaskProvider(this.storage){
+    _loadTasksFromStorage();
+  }
+
+  void _loadTasksFromStorage() async {
+    await storage.ready;
+    var storedTasks = storage.getItem('tasks');
+    if(storedTasks != null){
+      _tasks = List<Task>.from(
+        (storedTasks as List).map((item) => Task.fromJson(item)),
+      );
+       ();
+    }
+  }
+
+  void _saveTasksToStorage() {
+    String jsonString = jsonEncode(_tasks.map((e) => e.toJson()).toList());
+    storage.setItem('tasks', jsonString);
+  }
+
+  void addTasks(Task data) {
+    _tasks.add(data);
+    _saveTasksToStorage();
+    notifyListeners();
+  }
+}
---
+++ lib/providers/time_entry_provider.dart
@@ -0,0 +1,37 @@
+import 'package:localstorage/localstorage.dart';
+import 'package:flutter/material.dart';
+import '../models/time_entry.model.dart';
+import 'dart:convert';
+
+class TimeEntryProvider with ChangeNotifier {
+  LocalStorage storage;
+  List<TimeEntry> _timeEntries = [];
+
+  List<TimeEntry> get timeEntry => _timeEntries;
+
+  TimeEntryProvider(this.storage){
+    _loadTimeEntriesFromStorage();
+  }
+
+  void _loadTimeEntriesFromStorage() async {
+    await storage.ready;
+    var storedTimeEntries = storage.getItem('timeEntries');
+    if(storedTimeEntries != null){
+      _timeEntries = List<TimeEntry>.from(
+        (storedTimeEntries as List).map((item) => TimeEntry.fromJson(item)),
+      );
+       ();
+    }
+  }
+
+  void _saveTimeEntriesToStorage() {
+    String jsonString = jsonEncode(_timeEntries.map((e) => e.toJson()).toList());
+    storage.setItem('timeEntries', jsonString);
+  }
+
+  void addTimeEntry(TimeEntry data) {
+    _timeEntries.add(data);
+    _saveTimeEntriesToStorage();
+    notifyListeners();
+  }
+}
